0f27135165bddba8f29cbe8de7353732636ed32e
Jarod <jarod@Macbook-Pro-15.local>
2026-05-19 02:15:45 -0700

    fix: export default handler in off-search for ESM compatibility on Vercel

--- api/off-search.js
+++ api/off-search.js
@@ -21,7 +21,7 @@ function isEnglish(p) {
   return true;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
